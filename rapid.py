7548dd37f315fad0cd3c1d989bfac604d2a8f591
Jorik de Vries <jorik.de.vries@gmail.com>
2023-03-22 22:07:48 +0100

    threading

--- rapid.py
+++ rapid.py
@@ -1,4 +1,9 @@
 from ..bot_control import Move
+import gc
+import time
+import threading
+import numpy as np
+import weakref
 
 UP = Move.UP
 DOWN = Move.DOWN
@@ -12,6 +17,21 @@ SECOND_ZERO = UP
 SECOND_ELSE = DOWN
 
 
+def loop(rothko):
+    obj = rothko()
+    prev_pos = obj.position
+    last_update = time.time()
+    while obj and obj.c_round < obj.n_rounds:
+        if not np.array_equal(obj.position, prev_pos):
+            obj.next = obj.decide_move()
+            prev_pos = obj.position
+            last_update = time.time()
+
+        obj = None
+        obj = rothko()
+        time.sleep(0.01)
+
+
 class RapidRothko:
     def __init__(self):
         self._size = 0
@@ -19,32 +39,27 @@ class RapidRothko:
         self._move_hor = LEFT
         self._first_iter = True
 
-        self._idx_one = 0
-        self._idx_two = 1
+        self.position = np.array([0, 0], dtype=np.int16)
 
-    def get_name(self):
-        return "RapidRothko"
+        self.n_rounds = 10000
+        self.c_round = 0
 
-    def get_contributor(self):
-        return "Jorik de Vries"
+        self._update_thread = threading.Thread(target=loop, args=(weakref.ref(self),))
+        self._update_thread.start()
 
-    def determine_next_move(self, grid, enemies, game_info):
+        self.next = Move.LEFT
+
+    def decide_move(self):
         _x = self.position[0]
-
         _reached_zero = _x == 0
 
         if _reached_zero or _x == self._size:
             if self._first_iter:
-                if not self._size:
-                    self._size = grid.shape[0] - 1
-
                 self._move_hor = RIGHT if _reached_zero else LEFT
-
                 _y = self.position[1]
 
                 if _y == 0:
                     self._move_ver = UP
-
                 elif _y == self._size:
                     self._move_ver = DOWN
 
@@ -53,3 +68,16 @@ class RapidRothko:
 
             self._first_iter = True
         return self._move_hor
+
+    def get_name(self):
+        return "RapidRothko"
+
+    def get_contributor(self):
+        return "Jorik de Vries"
+
+    def determine_next_move(self, grid, enemies, game_info):
+        if not self._size:
+            self._size = grid.shape[0] - 1
+            self.n_rounds = game_info.number_of_rounds
+        self.c_round = game_info.current_round
+        return self.next
