610f97c6a0e6ac977904afe0d6c9d6096a874a7f
Jorik de Vries <jorik.de.vries@gmail.com>
2023-03-22 09:24:38 +0100

    small updates to rapid

--- rapid.py
+++ rapid.py
@@ -1,4 +1,3 @@
-import random
 from ..bot_control import Move
 
 UP = Move.UP
@@ -6,15 +5,22 @@ DOWN = Move.DOWN
 LEFT = Move.LEFT
 RIGHT = Move.RIGHT
 
-SIZE = 0
+FIRST_ZERO = RIGHT
+FIRST_ELSE = LEFT
+
+SECOND_ZERO = UP
+SECOND_ELSE = DOWN
 
 
 class RapidRothko:
     def __init__(self):
-        self._hor_move = None
-        self._ver_move = None
-
+        self._size = 0
+        self._move_ver = UP
+        self._move_hor = LEFT
         self._first_iter = True
+
+        self._idx_one = 0
+        self._idx_two = 1
 
     def get_name(self):
         return "RapidRothko"
@@ -24,32 +30,26 @@ class RapidRothko:
 
     def determine_next_move(self, grid, enemies, game_info):
         _x = self.position[0]
-        if self._hor_move is None:
-            self._hor_move = RIGHT if _x == 0 else LEFT
-            self._ver_move = UP if self.position[1] == 0 else DOWN
-            global SIZE
-            SIZE = grid.shape[0] - 1
 
-        _reached_left = _x == 0
-        _reached_right = _x == SIZE
+        _reached_zero = _x == 0
 
-        if _reached_left or _reached_right:
-            _y = self.position[1]
-            if _y == 0:
-                self._ver_move = UP
-            elif _y == SIZE:
-                self._ver_move = DOWN
+        if _reached_zero or _x == self._size:
+            if self._first_iter:
+                if not self._size:
+                    self._size = grid.shape[0] - 1
 
-            if _reached_left:
-                if self._first_iter:
-                    self._hor_move = RIGHT
-                    self._first_iter = False
-                    return self._ver_move
-                self._first_iter = True
-            elif _reached_right:
-                if self._first_iter:
-                    self._hor_move = LEFT
-                    self._first_iter = False
-                    return self._ver_move
-                self._first_iter = True
-        return self._hor_move
+                self._move_hor = RIGHT if _reached_zero else LEFT
+
+                _y = self.position[1]
+
+                if _y == 0:
+                    self._move_ver = UP
+
+                elif _y == self._size:
+                    self._move_ver = DOWN
+
+                self._first_iter = False
+                return self._move_ver
+
+            self._first_iter = True
+        return self._move_hor
